7675ff174c245345b3504e3872f85849cac3c488
João Vítor Carvalho <mistingerr@gmail.com>
2021-12-02 20:14:23 -0300

    added-swagger-helpers

---
+++ TalkToAPI/Helpers/Swagger/ApiVersionOperationFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TalkToAPI.Helpers.Swagger
+{
+    public static class ActionDescriptorExtentions
+    {
+        public static ApiVersionModel GetApiVersion(this ActionDescriptor actionDescriptor)
+        {
+            return actionDescriptor?.Properties.Where((kvp) => ((Type)kvp.Key).Equals(typeof(ApiVersionModel))).Select(kvp => kvp.Value as ApiVersionModel).FirstOrDefault();
+        }
+    }
+}

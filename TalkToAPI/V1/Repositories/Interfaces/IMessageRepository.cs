ce87f6e0a40151e98640522ee442858890e2167b
João Vítor Carvalho <mistingerr@gmail.com>
2021-12-03 14:39:29 -0300

    added-message controller

---
+++ TalkToAPI/V1/Repositories/Interfaces/IMessageRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TalkToAPI.V1.Models;
+
+namespace TalkToAPI.V1.Repositories.Interfaces
+{
+    public class IMessageRepository
+    {
+        List<Message> GetMessages(string owner, string receiver);
+
+        void Register(Message message);
+    }
+}
